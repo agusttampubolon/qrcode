97da0a305562e39cbeec5835c8a98f8878ad0d26
tampubolon agust <agust.tampubolon@live.com>
2019-03-26 22:41:26 +0700

    initial project

---
+++ SeinnovationDay/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace SeinnovationDay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.textBox2.Location = new System.Drawing.Point(212, 478);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(297, 31);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.webBrowser1.Location = new System.Drawing.Point(130, 48);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(470, 433);
+            this.webBrowser1.TabIndex = 2;
+            this.webBrowser1.Url = new System.Uri("https://seinnovationday.co.id/checkin/data?unique_code=JKT190321", System.UriKind.Absolute);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 442);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(717, 617);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.lblInput);
+            this.panel2.Controls.Add(this.btnClear);
+            this.panel2.Controls.Add(this.btn0);
+            this.panel2.Controls.Add(this.btn9);
+            this.panel2.Controls.Add(this.btnOK);
+            this.panel2.Controls.Add(this.btn5);
+            this.panel2.Controls.Add(this.btn6);
+            this.panel2.Controls.Add(this.btn8);
+            this.panel2.Controls.Add(this.btn4);
+            this.panel2.Controls.Add(this.btn7);
+            this.panel2.Controls.Add(this.btn3);
+            this.panel2.Controls.Add(this.btn2);
+            this.panel2.Controls.Add(this.btn1);
+            this.panel2.Location = new System.Drawing.Point(212, 34);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(317, 364);
+            this.panel2.TabIndex = 4;
+            this.panel2.Visible = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(40, 273);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 58);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(118, 273);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(75, 58);
+            this.btn0.TabIndex = 10;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(196, 88);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(75, 58);
+            this.btn9.TabIndex = 9;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(118, 149);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 58);
+            this.btn5.TabIndex = 5;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(195, 150);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 58);
+            this.btn6.TabIndex = 6;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(119, 88);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 58);
+            this.btn8.TabIndex = 8;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(40, 149);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 58);
+            this.btn4.TabIndex = 4;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(40, 87);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 58);
+            this.btn7.TabIndex = 7;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(195, 212);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 58);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(118, 211);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 58);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(40, 210);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 58);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(195, 273);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 58);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.BackColor = System.Drawing.Color.ForestGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(212, 529);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(297, 53);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Check In";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Calibri", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInput.ForeColor = System.Drawing.Color.White;
+            this.lblInput.Location = new System.Drawing.Point(93, 36);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(0, 33);
+            this.lblInput.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(44, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 33);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "JKT";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(212, 529);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(297, 55);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Show Keyboard";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SeinnovationDay.Properties.Resources.bg_app;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(717, 1059);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ SeinnovationDay/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeinnovationDay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 6000;
+            timer1.Enabled=true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!panel2.ContainsFocus)
+            {
+                textBox2.Focus();
+            }
+            //textBox1.Focus();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //textBox1.TextChanged += TextBox1_TextChanged;
+            //textBox1.Focus();
+            textBox2.LostFocus += TextBox2_LostFocus;
+            panel2.Hide();
+             
+        }
+
+        private void TextBox2_LostFocus(object sender, EventArgs e)
+        {
+            if (this.textBox2.Text != "")
+            {
+                call(this.textBox2.Text);
+            }
+           
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+            //MessageBox.Show(this.textBox2.Text);
+            //call(this.textBox2.Text);
+        }
+
+        public void call(string text)
+        {
+            if (text != "")
+            {
+                panel2.Hide();
+                
+                webBrowser1.Navigate("https://seinnovationday.co.id/checkin/printdata?unique_code=" + text);
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if(lblInput.Text!= "")
+            {
+                call("JKT" + lblInput.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please type your ID");
+            }
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            lblInput.Text = lblInput.Text + "0";
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            lblInput.Text = lblInput.Text + "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            lblInput.Text = lblInput.Text + "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            lblInput.Text = lblInput.Text + "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            lblInput.Text = lblInput.Text + "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            lblInput.Text = lblInput.Text + "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            lblInput.Text = lblInput.Text + "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            lblInput.Text = lblInput.Text + "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            lblInput.Text = lblInput.Text + "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            lblInput.Text = lblInput.Text + "9";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel2.Show();
+        }
+    }
+}
